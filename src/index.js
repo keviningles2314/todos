4c4b8645d1ad218c88b17283b2cc7f1ee9633965
keviningles2314 <kevin.ingles2314@gmail.com>
2021-12-08 18:03:14 -0600

    log deleted

--- src/index.js
+++ src/index.js
@@ -8,8 +8,3 @@ import { crearTodoHtml } from './js/components';
 export const todoList = new TodoList();
 
 todoList.todos.forEach(crearTodoHtml);
-
-// const newTodo = new Todo("aprender javascript");
-// todoList.nuevoTodo(newTodo);
-
-console.log('Todos', todoList.todos);
